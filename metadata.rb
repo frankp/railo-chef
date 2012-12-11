970589791758f7ce70b20e945994e3e2afac62f6
frankp <frank@clicknames.com.au>
2012-12-11 22:57:55 +1100

    Base Berks cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "railo-dev"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures railo-dev"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: railo-dev
+# Recipe:: default
+#
+# Copyright (C) 2012 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
